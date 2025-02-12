ad17c107125f1a38a26da32fb7629b7c1d2fa626
srihithamedi <medisrihitha05@gmail.com>
2025-02-12 10:14:38 +0530

    .

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ let user = {}
 // let username = "";
 // let currBalance = 0;
 document.write("<div id=root></div>");
+function toggle(color){
+  if(color=='dark'){
+    document.body.style.background="black";
+    document.body.style.color="white";
+  }
+  else{
+    document.body.style.background="white";
+    document.body.style.color="black";
+  }
+}
 function showUser() {
   if (document.getElementById("type").value == "3") {
     console.log("Transfer")
